b131bf064e2bdce30606dbc48219d557a3c9c54b
Luke Fisk-Lennon <luke.fisklennon@gmail.com>
2019-01-12 17:44:43 +1100

    Testing animation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var PIXI = require("./pixi.min.js");
 require("./index.scss");
 
-var game = new PIXI.Application({backgroundColor:0xffffff});
+var game = new PIXI.Application();
 document.body.appendChild(game.view);
 
 function resize() {
@@ -9,3 +9,24 @@ function resize() {
 }
 window.onresize = resize;
 resize();
+
+PIXI.loader.add(require("./sprites/sprite-image-horizontal.png"));
+PIXI.loader.load(() => {
+	var texture = PIXI.Texture.from(require("./sprites/sprite-image-horizontal.png"));
+	var frames = [];
+	for (var i = 0; i < 14; i++) {
+		frames.push({texture: new PIXI.Texture(texture, new PIXI.Rectangle(420 * i, 0, 420, 500)), time: 100});
+	}
+
+	var sprite = new PIXI.extras.AnimatedSprite(frames);
+
+	sprite.anchor.set(0.5);
+
+	sprite.x = game.screen.width / 2;
+	sprite.y = game.screen.height / 2;
+
+	// sprite.animationSpeed = 0.1;
+	sprite.play();
+
+	game.stage.addChild(sprite);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,28 +4,36 @@ var HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
 	mode: "development",
 	output: {
-		filename: "bundle.js",
+		filename: "[contenthash].js",
 		path: path.resolve(__dirname, "dist")
 	},
 	module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ]
-        },
-		{
-			test: /\.m?js$/,
-			exclude: /(node_modules|bower_components)/,
-			use: {
-				loader: "babel-loader",
-				options: {
-					presets: ["@babel/preset-env"]
+        rules: [
+			{
+	            test: /\.scss$/,
+	            use: [
+	                "style-loader",
+	                "css-loader",
+	                "sass-loader"
+	            ]
+	        },
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"]
+					}
 				}
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					"file-loader"
+				]
 			}
-		}]
+		]
     },
 	plugins: [
 		new HtmlWebpackPlugin({
